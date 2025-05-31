36deabc62131041edceee86cd91719f04260d935
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-31 18:44:51 +0600

    Fix import statements and register custom User model in admin

--- User/admin.py
+++ User/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-fron django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+admin.site.register(User, UserAdmin)
 
 # Register your models here.
