ea33563a426f68f3cda6b6f42b230a2835fdff62
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-31 18:43:50 +0600

    Add custom user model extending AbstractUser

--- User/models.py
+++ User/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+    """
+    Custom user model that extends the default Django user model.
+    You can add additional fields here if needed.
+    """
+    # Example of an additional field
+    # bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
 # Create your models here.
