2266656b9a0f08d47df081b443e19be5c1434d90
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-31 23:56:29 +0600

    Refactor URL patterns to include specific user-related endpoints

--- User/urls.py
+++ User/urls.py
@@ -5,5 +5,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('User.urls')),  # Include User app URLs
+    path('api/users/', include('users.urls')),
+    path('api/chamber/', include('chamber.urls')),
+    path('api/medicine/', include('medicine.urls')),
+    path('api/service/', include('chamberservice.urls')),
+    path('api/appointments/', include('appointment.urls')),
+    path('api/prescriptions/', include('prescription.urls')),
+    path('api/system-menus/', include('systemmenu.urls')),
 ]
