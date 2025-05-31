3bebd85a5065fc46ba82bb0f4c63944f6939db9f
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-31 22:01:08 +0600

    Add initial view and URL configuration for User app

--- User/views.py
+++ User/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
+from rest_framework import viewsets, permissions
+from .serializers import GroupSerializer, PermissionSerializer
+from .permissions import IsAdmin, IsDoctor, IsUser, IsAdminOrDoctor, is_doctor, is_admin, is_admin_or_doctor
+
 
 # Create your views here.
