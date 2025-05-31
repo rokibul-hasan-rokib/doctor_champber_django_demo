e0f562496aea82da3f3a325ab18eeba3f9047793
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-31 22:06:13 +0600

    Fix missing newline at end of file in RegisterUser view

--- User/views.py
+++ User/views.py
@@ -19,4 +19,4 @@ class RegisterUser(APIView):
         if serializer.is_valid():
             user = serializer.save()
             return Response({"message": "User created successfully", "user_id": user.id}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)                                                                                               
