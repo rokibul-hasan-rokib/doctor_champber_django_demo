edfdd59f172c37cc1a9536680f389c660ba80cf7
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-31 22:04:17 +0600

    Add RegisterUser API view for user registration

--- User/views.py
+++ User/views.py
@@ -9,6 +9,14 @@ from django.contrib.auth.models import Group, Permission
 from rest_framework import viewsets, permissions
 from .serializers import GroupSerializer, PermissionSerializer
 from .permissions import IsAdmin, IsDoctor, IsUser, IsAdminOrDoctor, is_doctor, is_admin, is_admin_or_doctor
-
+from rest_framework.decorators import action
 
 # Create your views here.
+
+class RegisterUser(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully", "user_id": user.id}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
