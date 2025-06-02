1d9fcb1efaa1611419aa704c3dd9b787e19974c7
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-06-03 00:41:15 +0600

    Add MedicineSerializer with manufacturer and creator fields

--- medicine/serializers.py
+++ medicine/serializers.py
@@ -11,3 +11,11 @@ class MedicineCompanyAdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = MedicineCompany
         fields = '__all__'
+
+class MedicineSerializer(serializers.ModelSerializer):
+    manufracturer_name = serializers.CharField(source='manufacturer.name', read_only=True)
+    created_by_name = serializers.ReadOnlyField(source='created_by.username')
+    class Meta:
+        model = Medicine
+        fields = '__all__'
+        read_only_fields = ['created_by', 'created_by_username', 'created_at', 'updated_at']
