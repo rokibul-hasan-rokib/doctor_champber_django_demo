ac81ffbbdc2c7a73af35f73e897d9359cba9b57d
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-06-03 00:46:53 +0600

    Add pagination and filtering setup for Medicine views

--- medicine/views.py
+++ medicine/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
+from .models import Medicine, MedicineCompany
+from .serializers import MedicineSerializer, MedicineCompanySerializer, MedicineCompanyAdminSerializer
+from users.permissions import (IsAuthenticatedOrAdminCreate, IsAdminOrReadOnly, is_admin,
+                               IsAdminOrDoctor, is_doctor)
+from rest_framework.exceptions import PermissionDenied
 
-# Create your views here.
+class MedicinePagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+
+
+
