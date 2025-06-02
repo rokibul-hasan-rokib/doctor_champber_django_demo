d2fe16756cc4be076a8b248f909cbee01856bb6c
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-06-03 00:34:10 +0600

    Add MedicineCompany model with fields for name, description, image, creator, and timestamp

--- medicine/models.py
+++ medicine/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+
+class MedicineCompany(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='medicine_companies/', blank=True, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='medicine_companies')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
