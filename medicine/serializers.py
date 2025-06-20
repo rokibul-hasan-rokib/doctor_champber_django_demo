c9e09095c2ec4a057bff2d7c1cb3de2033936350
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-06-20 23:53:14 +0600

    Remove create method and read_only_fields from MedicineCompanyAdminSerializer

--- medicine/serializers.py
+++ medicine/serializers.py
@@ -13,11 +13,7 @@ class MedicineCompanyAdminSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-    read_only_fields = ['created_by', 'created_at', 'updated_at']
-    def create(self, validated_data):
-        request = self.context.get('request')
-        validated_data['created_by'] = request.user
-        return super().create(validated_data)
+
 
 class MedicineSerializer(serializers.ModelSerializer):
     manufracturer_name = serializers.CharField(source='manufacturer.name', read_only=True)
